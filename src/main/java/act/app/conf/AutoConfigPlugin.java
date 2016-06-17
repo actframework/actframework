c1e4618054c1c60e2f46da8120ffc11e46784986
Gelin Luo <greenlaw110@gmail.com>
2016-06-17 11:15:20 +1000

    AutoConfig now support Const or $.Val type so we can safely use final notation

--- src/main/java/act/app/conf/AutoConfigPlugin.java
+++ src/main/java/act/app/conf/AutoConfigPlugin.java
@@ -4,14 +4,18 @@ import act.ActComponent;
 import act.app.App;
 import act.app.AppByteCodeScanner;
 import act.app.event.AppEventId;
+import act.conf.AppConfig;
 import act.event.AppEventListenerBase;
 import act.util.AnnotatedTypeFinder;
 import org.osgl.$;
 import org.osgl.exception.NotAppliedException;
+import org.osgl.util.Const;
 import org.osgl.util.E;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.EventObject;
 import java.util.Map;
 import java.util.Set;
@@ -65,13 +69,14 @@ public class AutoConfigPlugin extends AnnotatedTypeFinder {
     }
 
     private static class AutoConfigLoader {
-        private App app;
+        //private App app;
+        private AppConfig conf;
         private Class<?> autoConfigClass;
         private String ns;
         private static boolean setModifierField = false;
 
         AutoConfigLoader(App app, Class<?> autoConfigClass) {
-            this.app = app;
+            this.conf = app.config();
             this.autoConfigClass = autoConfigClass;
             this.ns = (autoConfigClass.getAnnotation(AutoConfig.class)).value();
             synchronized (AutoConfigLoader.class) {
@@ -131,11 +136,11 @@ public class AutoConfigPlugin extends AnnotatedTypeFinder {
 
         private void loadAutoConfig_(Field f, String ns) {
             String key = ns + "." + f.getName();
-            Object val = app.config().getIgnoreCase(key);
+            Object val = conf.getIgnoreCase(key);
             if (null == val) {
                 // try to change the "prefix.key_x_an_y" form to "prefix.key.x.an.y" form
                 key = key.replace('_', '.');
-                val = app.config().getIgnoreCase(key);
+                val = conf.getIgnoreCase(key);
                 if (null == val) {
                     return;
                 }
@@ -143,35 +148,52 @@ public class AutoConfigPlugin extends AnnotatedTypeFinder {
             Class<?> type = f.getType();
             boolean isFinal = false;
             try {
-                // we really want it to be public, so comment out: f.setAccessible(true);
                 isFinal = turnOffFinal(f);
-                if (String.class.equals(type)) {
-                    f.set(null, val);
-                } else if (Integer.TYPE.equals(type) || Integer.class.equals(type)) {
-                    f.set(null, Integer.parseInt(val.toString()));
-                } else if (Boolean.TYPE.equals(type) || Boolean.class.equals(type)) {
-                    f.set(null, Boolean.parseBoolean(val.toString()));
-                } else if (Character.TYPE.equals(type) || Character.class.equals(type)) {
-                    f.set(null, val.toString().charAt(0));
-                } else if (Byte.TYPE.equals(type) || Byte.class.equals(type)) {
-                    f.set(null, Byte.parseByte(val.toString()));
-                } else if (Long.TYPE.equals(type) || Long.class.equals(type)) {
-                    f.set(null, Long.parseLong(val.toString()));
-                } else if (Float.TYPE.equals(type) || Float.class.equals(type)) {
-                    f.set(null, Float.parseFloat(val.toString()));
-                } else if (Double.TYPE.equals(type) || Double.class.equals(type)) {
-                    f.set(null, Double.parseDouble(val.toString()));
-                } else if (Enum.class.isAssignableFrom(type)) {
-                    f.set(null, Enum.valueOf(((Class<Enum>) type), val.toString()));
-                } else {
-                    logger.warn("Config[%s] field type[%s] not recognized", key, type);
-                }
+                setField(f, null, key, val, type);
             } catch (Exception e) {
                 throw E.invalidConfiguration("Error get configuration " + key + ": " + e.getMessage());
             } finally {
                 if (isFinal) {
                     turnOnFinal(f);
                 }
+            }
+        }
+
+        private void setField(Field f, Object host, String key, Object val, Class<?> type) throws Exception {
+            if ($.Val.class.isAssignableFrom(type)) {
+                $.Val value = $.cast(f.get(host));
+                ParameterizedType type0 = $.cast(f.getGenericType());
+                Field fVal = $.Var.class.getDeclaredField("v");
+                fVal.setAccessible(true);
+                setField(fVal, value, key, val, (Class<?>) type0.getActualTypeArguments()[0]);
+                fVal.setAccessible(false);
+            } else if (Const.class.isAssignableFrom(type)) {
+                Const value = $.cast(f.get(host));
+                ParameterizedType type0 = $.cast(f.getGenericType());
+                Field fConst = Const.class.getDeclaredField("v");
+                fConst.setAccessible(true);
+                setField(fConst, value, key, val, (Class<?>) type0.getActualTypeArguments()[0]);
+                fConst.setAccessible(false);
+            } else if (String.class.equals(type)) {
+                f.set(host, val);
+            } else if (Integer.TYPE.equals(type) || Integer.class.equals(type)) {
+                f.set(host, Integer.parseInt(val.toString()));
+            } else if (Boolean.TYPE.equals(type) || Boolean.class.equals(type)) {
+                f.set(host, Boolean.parseBoolean(val.toString()));
+            } else if (Character.TYPE.equals(type) || Character.class.equals(type)) {
+                f.set(host, val.toString().charAt(0));
+            } else if (Byte.TYPE.equals(type) || Byte.class.equals(type)) {
+                f.set(host, Byte.parseByte(val.toString()));
+            } else if (Long.TYPE.equals(type) || Long.class.equals(type)) {
+                f.set(host, Long.parseLong(val.toString()));
+            } else if (Float.TYPE.equals(type) || Float.class.equals(type)) {
+                f.set(host, Float.parseFloat(val.toString()));
+            } else if (Double.TYPE.equals(type) || Double.class.equals(type)) {
+                f.set(host, Double.parseDouble(val.toString()));
+            } else if (Enum.class.isAssignableFrom(type)) {
+                f.set(host, Enum.valueOf(((Class<Enum>) type), val.toString()));
+            } else {
+                logger.warn("Config[%s] field type[%s] not recognized", key, type);
             }
         }
     }
