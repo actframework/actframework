fa1c915532acedd36df80b975aa663228cdaa72b
Gelin Luo <greenlaw110@gmail.com>
2016-10-02 18:38:50 +1100

    add loadContent to View class

--- src/main/java/act/view/View.java
+++ src/main/java/act/view/View.java
@@ -7,9 +7,12 @@ import act.conf.AppConfig;
 import act.plugin.AppServicePlugin;
 import act.plugin.Plugin;
 import act.util.ActContext;
+import org.osgl.util.C;
+import org.osgl.util.IO;
 import org.osgl.util.S;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * The base class that different View solution should extends
@@ -60,6 +63,22 @@ public abstract class View extends AppServicePlugin {
         }
         return templateHome;
     }
+
+    /**
+     * Load template content.
+     *
+     * This method is used by error reporting feature when app running in dev mode
+     * @param template the template path
+     * @return the template content in lines
+     */
+    protected List<String> loadContent(String template) {
+        File file = new File(templateRootDir(), template);
+        if (file.exists() && file.canRead()) {
+            return IO.readLines(file);
+        }
+        return C.list();
+    }
+
 
     protected final File templateRootDir() {
         App app = Act.app();
