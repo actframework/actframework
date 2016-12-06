40bf22385d0a705f4a94640a363985c9e6648b4f
Gelin Luo <greenlaw110@gmail.com>
2016-12-06 22:34:03 +0800

    add ~pid endpoint

--- src/main/java/act/Info.java
+++ src/main/java/act/Info.java
@@ -1,5 +1,6 @@
 package act;
 
+import act.sys.Env;
 import act.util.Banner;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.result.Result;
@@ -11,6 +12,11 @@ public class Info {
     @GetAction("/~info")
     public static Result show() {
         return text(Banner.cachedBanner());
+    }
+
+    @GetAction("/~pid")
+    public static Result pid() {
+        return text(Env.PID.get());
     }
 
 }
