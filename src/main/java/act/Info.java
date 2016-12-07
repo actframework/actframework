322184c813c7c27d42232958ab05f9b01c1067b4
Gelin Luo <greenlaw110@gmail.com>
2016-12-07 08:04:34 +0800

    support get info and pid in JSON format

--- src/main/java/act/Info.java
+++ src/main/java/act/Info.java
@@ -1,21 +1,39 @@
 package act;
 
+import act.app.ActionContext;
+import act.app.App;
 import act.sys.Env;
 import act.util.Banner;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.result.Result;
 
+import static act.controller.Controller.Util.jsonMap;
 import static act.controller.Controller.Util.text;
 
 public class Info {
 
     @GetAction("/~info")
-    public static Result show() {
-        return text(Banner.cachedBanner());
+    public static Object show(ActionContext context, App app) {
+        if (context.acceptJson()) {
+            String actVersion = Version.version();
+            String appVersion = Version.appVersion();
+            String appName = app.name();
+            String pid = Env.PID.get();
+            String baseDir = app.base().getAbsolutePath();
+            String mode = Act.mode().name();
+            String profile = Act.profile();
+            String group = Act.nodeGroup();
+            return jsonMap(actVersion, appVersion, appName, pid, baseDir, mode, profile, group);
+        }
+        return Banner.cachedBanner();
     }
 
     @GetAction("/~pid")
-    public static Result pid() {
+    public static Object pid(ActionContext context) {
+        if (context.acceptJson()) {
+            String pid = Env.PID.get();
+            return jsonMap(pid);
+        }
         return text(Env.PID.get());
     }
 
