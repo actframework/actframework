a52c09dc23f584ef5d459a2b28f3848d4411f54b
Gelin Luo <greenlaw110@gmail.com>
2016-12-23 22:14:43 +0800

    Job context transfer: now H.Session can be transferred

--- src/main/java/act/job/JobContext.java
+++ src/main/java/act/job/JobContext.java
@@ -1,5 +1,6 @@
 package act.job;
 
+import act.app.ActionContext;
 import act.util.ActContext;
 import org.osgl.http.H;
 
@@ -18,6 +19,12 @@ public class JobContext {
         ActContext<?> actContext = ActContext.Base.currentContext();
         if (null != actContext) {
             bag_.put("locale", actContext.locale());
+            if (actContext instanceof ActionContext) {
+                H.Session session = ((ActionContext) actContext).session();
+                if (null != session) {
+                    bag_.put("session", session);
+                }
+            }
         }
     }
 
@@ -27,7 +34,7 @@ public class JobContext {
 
     /**
      * Whether JobContext of current thread initialized
-     * @return
+     * @return `true` if current job context is not null
      */
     public static boolean initialized() {
         return null != current_.get();
@@ -54,17 +61,20 @@ public class JobContext {
 
     /**
      * Get value by key from the JobContext of current thread
-     * @param key
+     * @param key the key
+     * @param <T> the val type
      * @return
      */
-    public static Object get(String key) {
-        return m().get(key);
+    public static <T> T get(String key) {
+        return (T) m().get(key);
     }
 
     /**
      * Generic version of getting value by key from the JobContext of current thread
-     * @param key
-     * @return
+     * @param key the key
+     * @param clz the val class
+     * @param <T> the val type
+     * @return the value
      */
     public static <T> T get(String key, Class<T> clz) {
         return (T)m().get(key);
@@ -72,8 +82,8 @@ public class JobContext {
 
     /**
      * Set value by key to the JobContext of current thread
-     * @param key
-     * @return
+     * @param key the key
+     * @param val the value
      */
     public static void put(String key, Object val) {
         m().put(key, val);
@@ -81,8 +91,7 @@ public class JobContext {
 
     /**
      * Remove value by key from the JobContext of current thread
-     * @param key
-     * @return
+     * @param key the key
      */
     public static void remove(String key) {
         m().remove(key);
@@ -90,7 +99,7 @@ public class JobContext {
 
     /**
      * Make a copy of JobContext of current thread
-     * @return
+     * @return the copy of current job context or an empty job context
      */
     static JobContext copy() {
         JobContext ctxt = new JobContext();
@@ -103,7 +112,7 @@ public class JobContext {
 
     /**
      * Initialize current thread's JobContext using specified copy
-     * @param origin
+     * @param origin the original job context
      */
     static void init(JobContext origin) {
         current_.set(origin);
@@ -115,6 +124,10 @@ public class JobContext {
             Locale locale = (Locale) origin.bag_.get("locale");
             if (null != locale) {
                 actContext.locale(locale);
+            }
+            H.Session session = (H.Session) origin.bag_.get("session");
+            if (null != session) {
+                actContext.attribute("__session", session);
             }
         }
     }
