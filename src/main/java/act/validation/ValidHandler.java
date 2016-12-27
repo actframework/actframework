750b90e339bda99e4b53c5477793f9bc8167e6af
Genkyo <leeaee@gmail.com>
2016-12-27 15:16:24 +0800

    Add @NotBlank support in Javabean object.

--- src/main/java/act/validation/ValidHandler.java
+++ src/main/java/act/validation/ValidHandler.java
@@ -3,13 +3,15 @@ package act.validation;
 import act.ActComponent;
 import act.app.ActionContext;
 import act.controller.ActionMethodParamAnnotationHandlerPlugin;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.AggregateResourceBundleLocator;
 import org.osgl.util.C;
 
 import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -21,8 +23,10 @@ public class ValidHandler extends ActionMethodParamAnnotationHandlerPlugin {
     private static Validator validator;
 
     static {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+//        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = Validation.byDefaultProvider().configure()
+                .messageInterpolator(new ResourceBundleMessageInterpolator(new AggregateResourceBundleLocator(Arrays.asList("messages", "ValidationMessages"))))
+                .buildValidatorFactory().getValidator();
     }
 
     public Set<Class<? extends Annotation>> listenTo() {
@@ -32,11 +36,12 @@ public class ValidHandler extends ActionMethodParamAnnotationHandlerPlugin {
     }
 
     public void handle(String paramName, Object paramVal, Annotation annotation, ActionContext context) {
-        if(paramVal != null) {
+        if (null != paramVal) {
             Set constraintViolations = validator.validate(paramVal);
             if (!constraintViolations.isEmpty()) {
                 context.addViolations(constraintViolations);
             }
         }
     }
+
 }
