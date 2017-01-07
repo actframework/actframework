2ff3b298c66695e5550dbfc87e4b8d4a0d4b8723
Gelin Luo <greenlaw110@gmail.com>
2017-01-08 09:39:01 +1100

    Make act.db.ActiveRecord extends org.osgl.util.KV

--- src/main/java/act/db/ActiveRecord.java
+++ src/main/java/act/db/ActiveRecord.java
@@ -1,8 +1,10 @@
 package act.db;
 
+import org.osgl.util.KV;
+
 /**
  * The `ActiveRecord` interface specifies a special {@link Model} in that
  * the fields/columns could be implicitly defined by database
  */
-public interface ActiveRecord {
+public interface ActiveRecord extends KV {
 }
