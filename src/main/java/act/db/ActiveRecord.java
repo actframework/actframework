c712500e04c5a31d2db907da837ec438bd3db38e
Gelin Luo <greenlaw110@gmail.com>
2017-01-08 09:33:03 +1100

    add ActiveRecord tag interface

---
+++ src/main/java/act/db/ActiveRecord.java
@@ -0,0 +1,8 @@
+package act.db;
+
+/**
+ * The `ActiveRecord` interface specifies a special {@link Model} in that
+ * the fields/columns could be implicitly defined by database
+ */
+public interface ActiveRecord {
+}
