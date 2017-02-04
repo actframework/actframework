43e12f6a34a0549c8eda9a07f754a817dc70bf65
Gelin Luo <greenlaw110@gmail.com>
2017-02-04 16:11:34 +1100

    rename command name act.app.restart to act.restart, act.app.shutdown to act.shutdown

--- src/main/java/act/ActAdmin.java
+++ src/main/java/act/ActAdmin.java
@@ -1,22 +1,22 @@
-package act.app;
+package act;
 
-import act.Act;
+import act.app.App;
 import act.cli.CliContext;
 import act.cli.Command;
 
 /**
  * Admin interface to {@link App}
  */
-public class AppAdmin {
+public class ActAdmin {
 
-    @Command(value = "act.app.restart", help = "restart application node")
+    @Command(value = "act.restart", help = "restart application node")
     public void restart(CliContext context) {
         context.println("About to restart app. Your telnet session will be terminated. Please reconnect in a few seconds ...");
         context.flush();
         context.app().restart();
     }
 
-    @Command(value = "act.app.shutdown", help = "shutdown application node")
+    @Command(value = "act.shutdown", help = "shutdown application node")
     public void shutdown(CliContext context) {
         context.println("About to shutdown app. Your telnet session will be terminated. Please reconnect in a few seconds ...");
         context.flush();
