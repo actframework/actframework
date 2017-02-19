b9ce558b338217463ce61cb59ce7a47ec10baa1e
Gelin Luo <greenlaw110@gmail.com>
2017-02-19 11:06:19 +1100

    add more banner fonts

--- src/main/java/act/util/Banner.java
+++ src/main/java/act/util/Banner.java
@@ -5,12 +5,15 @@ import act.Zen;
 import act.conf.ConfLoader;
 import act.sys.Env;
 import com.github.lalyos.jfiglet.FigletFont;
+import org.osgl.$;
+import org.osgl.util.C;
 import org.osgl.util.E;
 import org.osgl.util.S;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * ASCII arts for Act
@@ -62,21 +65,38 @@ public class Banner {
         return sb.toString();
     }
 
+    private static final String[] _BANNER_FONTS = {
+            "banner3", "big", "marquee",
+            "mini", "slant", "small", "speed",
+            "standard", "starwars",
+    };
+
     private static String asciiArt(String s) {
         String font = System.getProperty("banner.font");
         if (null == font) {
             int len = s.length();
             if (len < 5) {
                 font = "big";
-            } else if (len < 8) {
+            } else if (len < 9) {
                 font = "standard";
-            } else if (len < 12) {
+            } else if (len < 13) {
                 font = "small";
             } else {
                 font = "mini";
             }
+        } else if ("BianLian".equals(font)) {
+            font = $.random(_BANNER_FONTS);
         }
-        InputStream is = Banner.class.getResourceAsStream("/" + font + ".flf");
+        String path = font.endsWith(".flf") ? font : S.builder("/").append(font).append(".flf").toString();
+        File file = new File(path);
+        if (file.exists() && file.canRead()) {
+            try {
+                return FigletFont.convertOneLine(file, s.toUpperCase());
+            } catch (IOException e) {
+                throw E.ioException(e);
+            }
+        }
+        InputStream is = Banner.class.getResourceAsStream(path);
         if (null == is) {
             is = Banner.class.getResourceAsStream("/standard.flf");
         }
@@ -108,6 +128,33 @@ public class Banner {
     }
 
     public static void main(String[] args) {
-        print("ACT", null);
+        List<String> fail = C.newList();
+        for (String font : _BANNER_FONTS) {
+            if (!test(font)) {
+                fail.add(font);
+            }
+        }
+        if (!fail.isEmpty()) {
+            System.out.println(">>>>>Failed:");
+            System.out.println(S.join(",", fail));
+        }
+    }
+
+    private static boolean test(String font) {
+        System.setProperty("banner.font", font);
+        try {
+            System.out.println("================= " + font + "==================");
+            printArt("ABCDEFGHIJKLM");
+            printArt("NOPQRSTUVWXYZ");
+            printArt("1234567890");
+            System.out.println("\n\n\n");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void printArt(String s) {
+        System.out.println(asciiArt(s));
     }
 }
