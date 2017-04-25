2eae51592bc71b16016f2b6dd60a7ded4ea7092b
Gelin Luo <greenlaw110@gmail.com>
2017-04-25 11:46:44 +1000

    #163 and #164 WIP

---
+++ src/main/java/act/controller/annotation/Port.java
@@ -0,0 +1,45 @@
+package act.controller.annotation;
+
+/*-
+ * #%L
+ * ACT Framework
+ * %%
+ * Copyright (C) 2014 - 2017 ActFramework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.lang.annotation.*;
+
+/**
+ * The annotation is used specify the {@link act.app.util.NamedPort port(s)}
+ * of a controller class
+ *
+ * **Note** this annotation is inherited
+ *
+ * See https://github.com/actframework/actframework/issues/164
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Target(ElementType.METHOD)
+public @interface Port {
+
+    /**
+     * Specify the ports of the controller class
+     *
+     * @return the name of the named ports
+     */
+    String[] value();
+
+}
