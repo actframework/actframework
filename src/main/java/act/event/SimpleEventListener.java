d7c69e083f450cb7173a335f52cbf0595f0990da
Gelin Luo <greenlaw110@gmail.com>
2017-05-30 16:12:59 +1000

    minor update in SimpleEventListener

--- src/main/java/act/event/SimpleEventListener.java
+++ src/main/java/act/event/SimpleEventListener.java
@@ -64,7 +64,7 @@ public interface SimpleEventListener {
      */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.ANNOTATION_TYPE)
-    public @interface Marker {
+    @interface Marker {
     }
 
 }
