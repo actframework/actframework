02d316a1deb2f4b383c58ee6c2c6055ea7e4b43f
Gelin Luo <greenlaw110@gmail.com>
2017-07-18 16:23:55 +1000

    fix #312

--- src/main/java/act/handler/builtin/controller/impl/ReflectedHandlerInvoker.java
+++ src/main/java/act/handler/builtin/controller/impl/ReflectedHandlerInvoker.java
@@ -502,7 +502,7 @@ public class ReflectedHandlerInvoker<M extends HandlerMethodMetaInfo> extends De
                     throw E.unexpected(e);
                 }
             }
-            context.fieldOutputVarCountt(outputFields.size());
+            context.fieldOutputVarCount(outputFields.size());
         }
         if (0 == params.length) {
             return;
