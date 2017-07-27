cf679b676b6288d306f792cb847ad8b67f614ba3
Gelin Luo <greenlaw110@gmail.com>
2017-07-28 09:50:59 +1000

    fix merge conflict

--- src/main/java/act/handler/builtin/controller/RequestHandlerProxy.java
+++ src/main/java/act/handler/builtin/controller/RequestHandlerProxy.java
@@ -45,6 +45,7 @@ import org.osgl.exception.UnexpectedException;
 import org.osgl.http.H;
 import org.osgl.logging.L;
 import org.osgl.logging.Logger;
+import org.osgl.mvc.result.BadRequest;
 import org.osgl.mvc.result.NotFound;
 import org.osgl.mvc.result.Result;
 import org.osgl.util.C;
