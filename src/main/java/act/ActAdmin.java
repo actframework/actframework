79c1ca109261292634108165186a3ca55f160e81
Gelin Luo <greenlaw110@gmail.com>
2017-08-06 13:03:24 +1000

    fix #321

--- src/main/java/act/ActAdmin.java
+++ src/main/java/act/ActAdmin.java
@@ -31,14 +31,14 @@ public class ActAdmin {
 
     @Command(value = "act.restart", help = "restart application node")
     public void restart(CliContext context) {
-        context.println("About to restart app. Your telnet session will be terminated. Please reconnect in a few seconds ...");
+        context.println(context._act_i18n("cli.restart"));
         context.flush();
         context.app().restart();
     }
 
     @Command(value = "act.shutdown", help = "shutdown application node")
     public void shutdown(CliContext context) {
-        context.println("About to shutdown app. Your telnet session will be terminated. Please reconnect in a few seconds ...");
+        context.println(context._act_i18n("cli.shutdown"));
         context.flush();
         Act.shutdownApp(context.app());
     }
