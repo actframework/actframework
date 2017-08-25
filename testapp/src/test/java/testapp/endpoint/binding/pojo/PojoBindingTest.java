ac77334a44069873fdbed0fab74d3acdc23f3c36
Gelin Luo <greenlaw110@gmail.com>
2017-08-26 08:24:26 +1000

    add PojoBindingTest for JSON data exchange

--- testapp/src/test/java/testapp/endpoint/binding/pojo/PojoBindingTest.java
+++ testapp/src/test/java/testapp/endpoint/binding/pojo/PojoBindingTest.java
@@ -1,8 +1,12 @@
 package testapp.endpoint.binding.pojo;
 
+import com.alibaba.fastjson.JSON;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.osgl.http.H;
 import org.osgl.util.C;
 import org.osgl.util.KVStore;
+import testapp.endpoint.EndPointTestContext;
 import testapp.endpoint.binding.ActionParameterBindingTestBase;
 import testapp.model.Contact;
 
@@ -13,10 +17,22 @@ public class PojoBindingTest extends ActionParameterBindingTestBase {
         return "/pojo";
     }
 
+    @Ignore
     @Test
     public void postFullContactMethodFormData() {
         Contact contact = prepareFullContact();
+        // TODO finish form data test
+    }
 
+    @Test
+    public void postFullContactJsonBody() throws Exception {
+        Contact contact = prepareFullContact();
+        context.expected(JSON.toJSONString(contact))
+                .url(processUrl("ctct"))
+                .accept(H.Format.JSON)
+                .jsonBody(C.map("contact", contact))
+                .method(EndPointTestContext.RequestMethod.POST_JSON)
+                .applyTo(this);
     }
 
     protected KVStore prepareNonEmptyKvStore() {
