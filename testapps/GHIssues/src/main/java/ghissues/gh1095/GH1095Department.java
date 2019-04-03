5c47abeb3bde9c4f40159b1f07ef7a15594e58c1
Gelin Luo <greenlaw110@gmail.com>
2019-04-03 21:43:29 +1100

    add testcase for GH1095

---
+++ testapps/GHIssues/src/main/java/ghissues/gh1095/GH1095Department.java
@@ -0,0 +1,9 @@
+package ghissues.gh1095;
+
+public class GH1095Department extends GH1095Base<GH1095Department> {
+    public String name;
+
+    public GH1095Department(String name) {
+        this.name = name;
+    }
+}
