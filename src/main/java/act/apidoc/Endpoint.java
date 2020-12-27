1b468365bb21a22f2d91a29b0348ac9cbcdf75b6
dfish <>
2020-12-27 10:30:58 +0800

    fix #532

--- src/main/java/act/apidoc/Endpoint.java
+++ src/main/java/act/apidoc/Endpoint.java
@@ -9,9 +9,9 @@ package act.apidoc;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -642,6 +642,8 @@ public class Endpoint implements Comparable<Endpoint>, EndpointIdProvider {
 
     private String generateSampleQuery(BeanSpec spec, Map<String, Class> typeParamLookup, String bindName, Set<Type> typeChain, List<String> nameChain) {
         Class<?> type = spec.rawType();
+        typeParamLookup = new HashMap<>(typeParamLookup);
+        Generics.buildTypeParamImplLookup(type, typeParamLookup);
         String specName = spec.name();
         if (S.notBlank(specName)) {
             nameChain.add(specName);
